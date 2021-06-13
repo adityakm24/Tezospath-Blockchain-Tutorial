98a344be5b2196b4e8893a73d890739e40e7b452
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2021-06-14 00:23:13 +0530

    File upload

---
+++ account.js
@@ -0,0 +1,25 @@
+let FAUCET_KEY;
+export default FAUCET_KEY ={
+  "mnemonic": [
+    "taste",
+    "veteran",
+    "nominee",
+    "bring",
+    "army",
+    "tonight",
+    "mystery",
+    "fury",
+    "fluid",
+    "wet",
+    "margin",
+    "own",
+    "feel",
+    "cancel",
+    "clean"
+  ],
+  "secret": "158d1ab4803f04b9390b8533c9c505532b9d96cc",
+  "amount": "1843315225",
+  "pkh": "tz1QoVv25U7CHbnArwsxSnznbTRJ7HbTCw3W",
+  "password": "IWznM9M3qK",
+  "email": "lnbizjsz.sqpehxxd@tezos.example.org"
+}
